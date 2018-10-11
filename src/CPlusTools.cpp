51a7057fc4a928814cf409e326b24592aae2cfc0
huzaichuangtianya <huzaichuangtianya@163.com>
2018-10-11 18:28:27 +0800

    Update CPlusTools.cpp
    update

--- src/CPlusTools.cpp
+++ src/CPlusTools.cpp
@@ -14,7 +14,7 @@ using namespace std;
 int main() {
 
 
-//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!!!
 
 	MyDictionary myd;
 
